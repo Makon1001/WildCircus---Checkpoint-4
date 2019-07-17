2ccdc8e03a8d77dd8b924f887ffb6f3b76fecacb
Manuel Konrath <manuel.konrath@gmail.com>
2019-07-17 18:07:53 +0200

    add secu

--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -10,8 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\FileUploader;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
 
 /**
+ * Require ROLE_ADMIN for *every* controller method in this class.
+ *
+ * @IsGranted("ROLE_ADMIN")
+ *
  * @Route("/actualite")
  */
 class ActualiteController extends AbstractController
